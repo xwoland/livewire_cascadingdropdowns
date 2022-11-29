dbbc63001d459295d4ee28807ee9b54d70d0e5f1
Alexander Borisov <Alexander.Borisov@emea.merkleinc.com>
2022-11-29 23:25:05 +0400

    Basic Cascading dropdown menu project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Continent;
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $continents = [
+            ['id' => 1, 'name' => 'Europe',],
+            ['id' => 2, 'name' => 'Asia',],
+            ['id' => 3, 'name' => 'Africa',],
+            ['id' => 4, 'name' => 'South America',],
+            ['id' => 5, 'name' => 'North America',],
+        ];
+        foreach ($continents as $continent) {
+            Continent::factory()->create($continent)
+                ->each(function ($c) {
+                    $c->countries()->saveMany(Country::factory(10)->make());
+                });
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
